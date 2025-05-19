4b9ef85c6848c450d514e895a712666e74952af7
JuanEstebanRodriguez0119 <143302182+JuanEstebanRodriguez0119@users.noreply.github.com>
2025-05-19 15:05:50 +0000

    V1.0 Streamlit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pymysql
 import os
+import streamlit as st
+import pandas as pd
+import pickle as pc
+from PIL import Image
 
 timeout = 10
 connection = pymysql.connect(
@@ -21,3 +25,26 @@ try:
   print(cursor.fetchall())
 finally:
   connection.close()
+
+
+
+st.title("Prediccion calidad de vinos🍷👌")
+st.write('------')
+st.header("Cargue un archivo csv con las variables Turbidez, Ph y caudal")
+st.subheader("Prediccion de vinos con el ph de mejor calidad👌")
+st.subheader("introducir grafico")
+st.write("Explicacion del grafico blablblabablablablablablablablablablablablablbalblablablablablahfidghhfkasyfigfjtfjgfjkjdgsjfvuash")
+st.subheader("Tipos de calidad de vino🍷")
+st.subheader("introducir grafico")
+st.write("Explicacion del grafico blablblabablablablablablablablablablablablablbalblablablablablahfidghhfkasyfigfjtfjgfjkjdgsjfvuash")
+st.header("¿Quieres saber la calidad que podria tener tu vino?😉")
+
+st.slider("hola",0,100,10)
+uploaded_file = st.file_uploader("Cargue su archivo csv" ,type=["csv"])
+
+if uploaded_file is not None:
+    input_dfd = pd.read_csv(uploaded_file,sep=";")
+
+    load_pred = pc.load(open('pr.pkl', 'rb'))
+    input_dfd['estimacion_efi'] = load_pred.predict(input_dfd)
+    st.write(input_dfd)
